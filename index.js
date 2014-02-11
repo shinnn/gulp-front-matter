13afb4862c66f3be8e262639941a18af39268b16
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-02-11 22:57:39 +0900

    Get null files through. Emit PluginError instead of Error.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var es = require('event-stream');
 var _ = require('lodash');
 var frontMatter = require('front-matter');
+var gutil = require('gulp-util');
+
+var PLUGIN_NAME = 'gulp-front-matter';
 
 module.exports = function (options) {
 
@@ -17,17 +20,19 @@ module.exports = function (options) {
       try {
         content = frontMatter(String(file.contents));
       } catch (e) {
-        return cb(e);
+        return cb(new gutil.PluginError(PLUGIN_NAME, e));
       }
 
       file[options.property] = content.attributes;
       if (options.remove) {
         file.contents = new Buffer(content.body);
       }
-    } else {
+    } else if (file.isNull()) {
+      return cb(null, file);
+    } else  {
       // stream
       // @ToDo implement the stream handling 
-      return cb(new Error('gulp-front-matter: Cannot get the front matter in a stream'), file);
+      return cb(new gutil.PluginError(PLUGIN_NAME, 'gulp-front-matter: Cannot get the front matter in a stream'));
     }
 
     cb(null, file);
