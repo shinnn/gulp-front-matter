ad652a38f83d8d20a89b4afafbc697b51066ba25
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-12-16 11:26:30 +0900

    Introduce linter tools (jscs and eslint)
    * https://github.com/jscs-dev/node-jscs
    * http://eslint.org/

--- test/gulp-front-matter.js
+++ test/gulp-front-matter.js
@@ -1,24 +1,22 @@
-"use strict";
+'use strict';
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var expect = require('chai').expect;
 
 var mapStream = require('map-stream');
-var fs = require('fs');
 var path = require('path');
 var stream = require('stream');
 
-var frontMatter = require('../');
+var frontMatter = require('..');
 
+var input = path.join(__dirname, 'fixtures/*.md');
 
-var input = __dirname + '/fixtures/*.md';
-
-function test (input, options, check) {
-  return function (done) {
+function test(input, options, check) {
+  return function(done) {
     gulp.src(input)
       // Keep original contents
-      .pipe(mapStream(function (file, cb) {
+      .pipe(mapStream(function(file, cb) {
         file.originalContents = file.contents;
         cb(null, file);
       }))
@@ -29,44 +27,43 @@ function test (input, options, check) {
   };
 }
 
-
 describe('gulp-front-matter', function() {
 
-  it('should extract front-matter header and modify contents', test(input, {}, function (file, cb) {
+  it('should extract front-matter header and modify contents', test(input, {}, function(file, cb) {
     expect(file.frontMatter).to.be.an('object').and.have.property('layout');
     expect(file.originalContents.length).to.be.greaterThan(file.contents.length);
     cb();
   }));
 
-  it('should keep contents unchanged if remove = false', test(input, {remove: false}, function (file, cb) {
+  it('should keep contents unchanged if remove = false', test(input, {remove: false}, function(file, cb) {
     expect(String(file.originalContents)).to.equal(String(file.contents));
     cb();
   }));
 
-  it('should be able to customize property name', test(input, {property: 'data'}, function (file, cb) {
-    expect(file.frontMatter).to.be.undefined;
+  it('should be able to customize property name', test(input, {property: 'data'}, function(file, cb) {
+    expect(file).to.not.have.property('frontMatter');
     expect(file.data).to.be.an('object').and.have.property('layout');
     cb();
   }));
 
-  it ('should raise a plugin error for stream file', function (done) {
-    var streamFile = new gutil.File({ contents: new stream.Stream() });
+  it('should raise a plugin error for stream file', function(done) {
+    var streamFile = new gutil.File({contents: new stream.Stream()});
     var fm = frontMatter()
-      .on('error', function (err) {
+      .on('error', function(err) {
         expect(err).to.be.an.instanceOf(gutil.PluginError);
         done();
       })
-      .on('end', function () {
+      .on('end', function() {
         done(new Error('Stream end without error'));
       });
     fm.write(streamFile);
     fm.end();
   });
 
-  it ('should get null file through', function (done) {
+  it('should get null file through', function(done) {
     var nullFile = new gutil.File();
     var fm = frontMatter()
-      .on('data', function (file) {
+      .on('data', function(file) {
         expect(file).to.be.equal(nullFile);
       })
       .on('end', done);
