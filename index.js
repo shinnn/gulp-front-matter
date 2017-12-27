4f2b40ee7adb983aaf037238bc91ad8fd4b54090
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-27 09:44:05 +0900

    update dependencies and devDependencies
    Highlights:
    
    * replace deprecated gulp-util with plugi-error
    * use built-in stream instead of readable-stream
    * update ESLint from v2 to v4
    * introduce @shinnn/eslint-config and follow its coding rules, for exmaple tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -2,60 +2,58 @@
 
 const frontMatter = require('front-matter');
 const objectPath = require('object-path');
-const PluginError = require('gulp-util/lib/PluginError');
-const Transform = require('readable-stream/transform');
-const tryit = require('tryit');
+const PluginError = require('plugin-error');
+const Transform = require('stream').Transform;
 const VinylBufferStream = require('vinyl-bufferstream');
 
 module.exports = function gulpFrontMatter(options) {
-  options = Object.assign({property: 'frontMatter'}, options);
+	options = Object.assign({property: 'frontMatter'}, options);
 
-  if (typeof options.property !== 'string') {
-    throw new PluginError('gulp-front-matter', new TypeError(
-      options.property +
-      ' is not a string. "property" option must be a string.'
-    ));
-  }
+	if (typeof options.property !== 'string') {
+		throw new PluginError('gulp-front-matter', new TypeError(`${
+			options.property
+		} is not a string. "property" option must be a string.`));
+	}
 
-  return new Transform({
-    objectMode: true,
-    transform(file, enc, cb) {
-      const run = new VinylBufferStream((buf, done) => {
-        let content;
+	return new Transform({
+		objectMode: true,
+		transform(file, enc, cb) {
+			const run = new VinylBufferStream((buf, done) => {
+				let content;
 
-        tryit(() => {
-          content = frontMatter(String(buf), {filename: file.path});
-          objectPath.set(file, options.property, content.attributes);
-        }, err => {
-          if (err) {
-            err.message = err.stack.replace(/\n +at[\s\S]*/, '');
-            const errorOption = {};
-            if (file.path !== undefined) {
-              errorOption.fileName = file.path;
-            }
-            done(new PluginError('gulp-front-matter', err, errorOption));
-            return;
-          }
+				try {
+					content = frontMatter(String(buf), {filename: file.path});
+					objectPath.set(file, options.property, content.attributes);
+				} catch (err) {
+					err.message = err.stack.replace(/\n +at[\s\S]*/, '');
+					const errorOption = {};
 
-          if (options.remove !== false) {
-            done(null, new Buffer(content.body));
-            return;
-          }
+					if (file.path !== undefined) {
+						errorOption.fileName = file.path;
+					}
 
-          done(null, buf);
-        });
-      });
+					done(new PluginError('gulp-front-matter', err, errorOption));
+					return;
+				}
 
-      run(file, (err, contents) => {
-        if (err) {
-          this.emit('error', err);
-        } else {
-          file.contents = contents;
-          this.push(file);
-        }
+				if (options.remove !== false) {
+					done(null, new Buffer(content.body));
+					return;
+				}
 
-        cb();
-      });
-    }
-  });
+				done(null, buf);
+			});
+
+			run(file, (err, contents) => {
+				if (err) {
+					this.emit('error', err);
+				} else {
+					file.contents = contents;
+					this.push(file);
+				}
+
+				cb();
+			});
+		}
+	});
 };
--- test.js
+++ test.js
@@ -1,145 +1,142 @@
 'use strict';
 
-const frontMatter = require('.');
+const gulpFrontMatter = require('.');
 const File = require('vinyl');
 const stringToStream = require('from2-string');
 const test = require('tape');
 
 test('gulp-front-matter', t => {
-  t.plan(19);
+	t.plan(18);
 
-  t.strictEqual(frontMatter.name, 'gulpFrontMatter', 'should have a function name.');
+	gulpFrontMatter()
+	.on('error', t.fail)
+	.on('data', file => {
+		t.deepEqual(file, new File(), 'should read null file as it is.');
+	})
+	.end(new File());
 
-  frontMatter()
-  .on('error', t.fail)
-  .on('data', file => {
-    t.deepEqual(file, new File(), 'should read null file as it is.');
-  })
-  .end(new File());
+	gulpFrontMatter()
+	.on('error', t.fail)
+	.on('data', ({contents, frontMatter}) => {
+		t.deepEqual(frontMatter, [true], 'should add `frontMatter` property to the file.');
+		t.deepEqual(contents, Buffer.from('Hi'), 'should remove front matter from a buffer.');
+	})
+	.end(new File({contents: Buffer.from('---\n- true\n---\nHi')}));
 
-  frontMatter()
-  .on('error', t.fail)
-  .on('data', file => {
-    t.deepEqual(file.frontMatter, [true], 'should add `frontMatter` property to the file.');
-    t.deepEqual(file.contents, new Buffer('Hi'), 'should remove front matter from a buffer.');
-  })
-  .end(new File({contents: Buffer.from('---\n[true]---\nHi')}));
+	gulpFrontMatter()
+	.on('error', t.fail)
+	.on('data', ({contents, frontMatter}) => {
+		t.deepEqual(
+			frontMatter,
+			{},
+			'should add `{}` to the `frontMatter` property when the buffer has no front matter.'
+		);
+		t.ok(
+			contents.equals(Buffer.from('--Hello--')),
+			'should not modify buffer which has no front matter.'
+		);
+	})
+	.end(new File({contents: Buffer.from('--Hello--')}));
 
-  frontMatter()
-  .on('error', t.fail)
-  .on('data', file => {
-    t.deepEqual(
-      file.frontMatter,
-      {},
-      'should add `{}` to the `frontMatter` property when the buffer has no front matter.'
-    );
-    t.deepEqual(
-      file.contents,
-      Buffer.from('--Hello--'),
-      'should not modify buffer which has no front matter.'
-    );
-  })
-  .end(new File({contents: Buffer.from('--Hello--')}));
+	gulpFrontMatter()
+	.on('error', t.fail)
+	.on('data', ({contents}) => {
+		t.deepEqual(
+			contents,
+			Buffer.alloc(0),
+			'should remove all the contents when the contents include nothing but front matter.'
+		);
+	})
+	.end(new File({contents: Buffer.from('---\na: 1\n---\n')}));
 
-  frontMatter()
-  .on('error', t.fail)
-  .on('data', file => {
-    t.deepEqual(
-      file.contents,
-      Buffer.from(''),
-      'should remove all the contents when the contents include nothing but front matter.'
-    );
-  })
-  .end(new File({contents: Buffer.from('---\na: 1\n---\n')}));
+	gulpFrontMatter()
+	.on('error', t.fail)
+	.on('data', file => {
+		t.deepEqual(
+			file.frontMatter,
+			{a: 1},
+			'should extract front matter data from a stream.'
+		);
+		file.contents
+		.on('error', t.fail)
+		.on('data', chunk => {
+			t.ok(chunk.equals(Buffer.from('Hi')), 'should remove front matter from stream.');
+		});
+	})
+	.end(new File({contents: stringToStream('---\na: 1\n---\nHi')}));
 
-  frontMatter()
-  .on('error', t.fail)
-  .on('data', file => {
-    t.deepEqual(
-      file.frontMatter,
-      {a: 1},
-      'should extract front matter data from a stream.'
-    );
-    file.contents
-    .on('error', t.fail)
-    .on('data', chunk => {
-      t.deepEqual(chunk, Buffer.from('Hi'), 'should remove front matter from stream.');
-    });
-  })
-  .end(new File({contents: stringToStream('---\na: 1\n---\nHi')}));
+	gulpFrontMatter({property: 'foo', remove: false})
+	.on('error', t.fail)
+	.on('data', file => {
+		t.deepEqual(
+			file.foo,
+			[false],
+			'should change property name using `property` option.'
+		);
+		t.notOk(
+			'frontMatter' in file,
+			'should not change `frontMatter` property when `property` option isn\'t the default value.'
+		);
+		t.deepEqual(
+			file.contents,
+			Buffer.from('---\n- false\n---\nHi'),
+			'should not modify contents when `remove` option is disabled.'
+		);
+	})
+	.end(new File({contents: Buffer.from('---\n- false\n---\nHi')}));
 
-  frontMatter({property: 'foo', remove: false})
-  .on('error', t.fail)
-  .on('data', file => {
-    t.deepEqual(
-      file.foo,
-      [false],
-      'should change property name using `property` option.'
-    );
-    t.notOk(
-      'frontMatter' in file,
-      'should not change `frontMatter` property when `property` option isn\'t the default value.'
-    );
-    t.deepEqual(
-      file.contents,
-      Buffer.from('---\n- false\n---\nHi'),
-      'should not modify contents when `remove` option is disabled.'
-    );
-  })
-  .end(new File({contents: Buffer.from('---\n- false\n---\nHi')}));
+	gulpFrontMatter({property: 'data.foo'})
+	.on('error', t.fail)
+	.on('data', ({data}) => {
+		t.deepEqual(
+			data,
+			{foo: [1]},
+			'should change deep properties using `property.subproperty` option.'
+		);
+	})
+	.end(new File({contents: Buffer.from('---\n- 1\n---\nHi')}));
 
-  frontMatter({property: 'data.foo'})
-  .on('error', t.fail)
-  .on('data', file => {
-    t.deepEqual(
-      file.data,
-      {foo: [1]},
-      'should change deep properties using `property.subproperty` option.'
-    );
-  })
-  .end(new File({contents: Buffer.from('---\n- 1\n---\nHi')}));
+	gulpFrontMatter()
+	.on('error', ({fileName, message}) => {
+		t.ok(
+			/.*line.*column/.test(message),
+			'should emit a plugin error when it cannot parse front matter in a buffer as YAML.'
+		);
+		t.equal(fileName, 'foo/bar.yaml', 'should include file name in the error.');
+	})
+	.end(new File({
+		path: 'foo/bar.yaml',
+		contents: Buffer.from('---\n:\n-{\n---')
+	}));
 
-  frontMatter()
-  .on('error', err => {
-    t.ok(
-      /.*line.*column/.test(err.message),
-      'should emit a plugin error when it cannot parse front matter in a buffer as YAML.'
-    );
-    t.strictEqual(err.fileName, 'foo/bar.yaml', 'should include file name in the error.');
-  })
-  .end(new File({
-    path: 'foo/bar.yaml',
-    contents: Buffer.from('---\n:\n-{\n---')
-  }));
+	gulpFrontMatter()
+	.on('error', err => {
+		t.ok(
+			/.*line.*column/.test(err.message),
+			'should emit a plugin error when it cannot parse front matter in a stream as YAML.'
+		);
+		t.notOk(
+			'fileName' in err,
+			'should not include file name in the error when the vinyl object doesn\'t have one.'
+		);
+	})
+	.end(new File({contents: stringToStream('---\n:\n-{\n---')}));
 
-  frontMatter()
-  .on('error', err => {
-    t.ok(
-      /.*line.*column/.test(err.message),
-      'should emit a plugin error when it cannot parse front matter in a stream as YAML.'
-    );
-    t.notOk(
-      'fileName' in err,
-      'should not include file name in the error when the vinyl object doesn\'t have one.'
-    );
-  })
-  .end(new File({contents: stringToStream('---\n:\n-{\n---')}));
+	const deepPropFile = new File({contents: Buffer.from('Hi')});
+	deepPropFile.data = true;
 
-  const deepPropFile = new File({contents: Buffer.from('Hi')});
-  deepPropFile.data = true;
+	gulpFrontMatter({property: 'data.foo'})
+	.on('error', err => {
+		t.ok(
+			/^TypeError.*true/.test(err.message),
+			'should emit a plugin error when it fails to set deep property.'
+		);
+	})
+	.end(deepPropFile);
 
-  frontMatter({property: 'data.foo'})
-  .on('error', err => {
-    t.ok(
-      /^TypeError.*true/.test(err.message),
-      'should emit a plugin error when it fails to set deep property.'
-    );
-  })
-  .end(deepPropFile);
-
-  t.throws(
-    frontMatter.bind(null, {property: 1}),
-    /.*is not a string.*must be a string/,
-    'should throw a plugin error when `property` option is not a string.'
-  );
+	t.throws(
+		() => gulpFrontMatter({property: 1}),
+		/.*is not a string.*must be a string/,
+		'should throw a plugin error when `property` option is not a string.'
+	);
 });
